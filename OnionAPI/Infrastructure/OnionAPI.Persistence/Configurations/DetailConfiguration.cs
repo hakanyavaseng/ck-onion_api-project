d7b847d1d3332e6ce7dbc7ab6b4992d17346245a
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-15 20:40:19 +0300

    Lecture_5: Entity configurations with IEntityTypeConfiguration and seeding data with Bogus.

---
+++ OnionAPI/Infrastructure/OnionAPI.Persistence/Configurations/DetailConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnionAPI.Domain.Entities;
+
+namespace OnionAPI.Persistence.Configurations
+{
+    public class DetailConfiguration : IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            Faker faker = new("tr");
+
+            builder.HasData(
+                new Detail
+                {
+                    Id = 1,
+                    Title = faker.Lorem.Sentence(1),
+                    Description = faker.Lorem.Sentence(5),
+                    CategoryId = 1,
+                    CreatedDate = DateTime.Now,
+                    IsDeleted = false
+                }, 
+                new Detail
+                {
+                    Id = 2,
+                    Title = faker.Lorem.Sentence(2),
+                    Description = faker.Lorem.Sentence(5),
+                    CategoryId = 3,
+                    CreatedDate = DateTime.Now,
+                    IsDeleted = true
+                },
+                 new Detail
+                 {
+                     Id = 3,
+                     Title = faker.Lorem.Sentence(2),
+                     Description = faker.Lorem.Sentence(5),
+                     CategoryId = 4,
+                     CreatedDate = DateTime.Now,
+                     IsDeleted = false
+                 });
+            
+        }
+    }
+}
