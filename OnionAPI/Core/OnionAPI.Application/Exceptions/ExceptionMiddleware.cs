cd8d86acd1da779c6876a6af07feb25c07eef4db
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-19 13:33:15 +0300

    Lecture_16: Microsoft Identity integration, User and Role entities added.

--- OnionAPI/Core/OnionAPI.Application/Exceptions/ExceptionMiddleware.cs
+++ OnionAPI/Core/OnionAPI.Application/Exceptions/ExceptionMiddleware.cs
@@ -49,7 +49,7 @@ namespace OnionAPI.Application.Exceptions
             exception switch
             {
                 BadRequestException => StatusCodes.Status400BadRequest,
-                NotFoundException => StatusCodes.Status400BadRequest,
+                NotFoundException => StatusCodes.Status404NotFound,
                 ValidationException => StatusCodes.Status422UnprocessableEntity,
                 _ => StatusCodes.Status500InternalServerError
             };
---
+++ OnionAPI/Core/OnionAPI.Domain/Entities/Identity/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnionAPI.Domain.Entities.Identity
+{
+    public class User : IdentityUser<Guid>
+    {
+        public  string FullName { get; set; }
+        public  string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpireDateTime { get; set; }
+    }
+}
