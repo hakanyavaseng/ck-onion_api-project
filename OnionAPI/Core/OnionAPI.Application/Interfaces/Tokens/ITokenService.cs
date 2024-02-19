abd4de5d3a9a0c6eebac5f1534e4aec9a67d7c4c
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-19 14:09:55 +0300

    Lecture_17: JWT Token Service implementation, CreateToken function.

---
+++ OnionAPI/Core/OnionAPI.Application/Interfaces/Tokens/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using OnionAPI.Domain.Entities.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace OnionAPI.Application.Interfaces.Tokens
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateToken(User user, IList<string> roles);
+        string GenerateRefreshToken();
+        ClaimsPrincipal? GetPrincipalFromExpiredToken();
+    }
+}
---
+++ OnionAPI/Infrastructure/OnionAPI.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnionAPI.Infrastructure.Tokens;
+
+namespace OnionAPI.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<TokenSettings>(configuration.GetSection("Key"));
+            
+
+        }
+    }
+}
