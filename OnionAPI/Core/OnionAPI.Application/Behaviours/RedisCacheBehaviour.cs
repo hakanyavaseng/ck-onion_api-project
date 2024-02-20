84d3e842a0291713d90879df7a9fccbba4b016d4
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-20 13:28:37 +0300

    Lecture_23: RedisCacheBehaviour, RedisCacheService and settings developed.

---
+++ OnionAPI/Core/OnionAPI.Application/Behaviours/RedisCacheBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using OnionAPI.Application.Interfaces.RedisCache;
+
+namespace OnionAPI.Application.Behaviours
+{
+    public class RedisCacheBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IRedisCacheService redisCacheService;
+
+        public RedisCacheBehaviour(IRedisCacheService redisCacheService)
+        {
+            this.redisCacheService = redisCacheService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if(request is ICacheableQuery query)
+            {
+                string? cacheKey = query.CacheKey;
+                double cacheTime = query.CacheTime;
+
+                TResponse cachedData = await redisCacheService.GetAsync<TResponse>(cacheKey);
+                if(cachedData is not null)
+                    return cachedData;
+
+                TResponse response = await next();
+
+                if (response is not null)
+                    await redisCacheService.SetAsync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime));
+
+                return response;
+            }
+            
+            return await next();
+        }
+    }
+}
